56382c71dbe847e30d2ef423757b344724390117
Vincent J. Margiotta Jr <vincent.joseph.margiotta.jr@gmail.com>
2021-08-24 12:26:11 -0400

    upload solution

---
+++ cses/tasks/1068/weird_algorithm.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+
+from typing import Callable
+from itertools import accumulate
+
+
+def hailstone(n: int) -> int:
+    return int(n/2) if n%2 == 0 else 3*n+1
+
+
+def apply(input: int, rule: Callable) -> int:
+    if input == 1:
+        return 1
+    return rule(input)
+
+
+def main(input: str, collector: list = []) -> str:
+    while True:
+        collector.append(input)
+        if input == '1':
+            break
+        input = str(apply(int(input), hailstone))
+    return ' '.join(collector)
+
+
+if __name__ == '__main__':
+    print(main(input()))
