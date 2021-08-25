4ddb72eb105dcd1d2760e37f94665fd2e15544b4
Vincent J. Margiotta Jr <vincent.joseph.margiotta.jr@gmail.com>
2021-08-25 12:11:15 -0400

    use integer division

--- cses/tasks/1068/missing_number.py
+++ cses/tasks/1068/missing_number.py
@@ -2,9 +2,9 @@
 
 
 def main(n: int, string_numbers: str) -> str:
-    nth_triangular = (0.5)*n*(n+1)  # AKA the sum of the integers up to n
+    nth_triangular = n*(n+1)//2  # AKA the sum of the integers up to n
     integer_list = [int(s) for s in string_numbers.split(' ')]
-    return int(nth_triangular - sum(integer_list))
+    return nth_triangular-sum(integer_list)
 
 
 if __name__ == '__main__':
