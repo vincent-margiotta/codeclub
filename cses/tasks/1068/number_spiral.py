a9140f23c636f7b43fc01c564906737ae81be67a
Vincent J. Margiotta Jr <vincent.joseph.margiotta.jr@gmail.com>
2021-09-09 16:53:50 -0400

    add solution

---
+++ cses/tasks/1068/number_spiral.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+def main(y: int, x: int) -> int:
+    m = y if y >= x else x
+    result = (m*m) - m + 1
+    offset = abs(y - x)
+    if m == y and m % 2 == 1:
+        offset *= -1
+    if m == x and m % 2 == 0:
+        offset *= -1
+    return result + offset
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        y, x = [int(s) for s in input().split(' ')]
+        print(main(y, x))
